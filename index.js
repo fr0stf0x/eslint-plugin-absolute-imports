dfbd883a4b17033cd1e5a91b19ec76abd67b9ddf
Thao Tang <thaotx3@vng.com.vn>
2022-04-21 10:07:38 +0700

    - Allow setting config path - Allow options 'minLevel' and 'jsOnly'

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const fs = require("fs");
 const path = require("path");
 
+const HAS_EXT_REGEX = /\.[a-z]+$/;
+const JS_EXT_REGEX = /\.(ts|tsx|js|jsx|json)$/;
+
 function has(map, path) {
   let inner = map;
   for (const step of path.split(".")) {
@@ -15,12 +18,26 @@ function has(map, path) {
   return true;
 }
 
+function getBaseUrlFromConfig(baseDir, configFilePath) {
+  let url = "";
+
+  const config = JSON.parse(
+    fs.readFileSync(configFilePath)
+  );
+
+  if (has(config, "compilerOptions.baseUrl")) {
+    url = config.compilerOptions.baseUrl;
+  }
+
+  return path.join(baseDir, url);
+}
+
 function findDirWithFile(filename) {
   let dir = path.resolve(filename);
 
   do {
     dir = path.dirname(dir);
-  } while (!fs.existsSync(path.join(dir, filename)) && dir !== "/");
+  } while (!fs.existsSync(path.join(dir, filename)) && dir !== "/" && dir !== process.cwd());
 
   if (!fs.existsSync(path.join(dir, filename))) {
     return;
@@ -51,18 +68,90 @@ function getBaseUrl(baseDir) {
   return path.join(baseDir, url);
 }
 
+const defaultOptions = {
+  jsOnly: true, // fix all files
+  minLevel: 1, // files in the same directory are ignored
+};
+
+const defaultSettings = {
+  project: '',
+};
+
 module.exports.rules = {
   "only-absolute-imports": {
     meta: {
       fixable: true,
     },
     create: function (context) {
-      const baseDir = findDirWithFile("package.json");
-      const baseUrl = getBaseUrl(baseDir);
+      const { options, settings } = context;
+
+      // Merge settings
+      let mergedSettings = defaultSettings;
+
+      if (settings['absolute-imports']) {
+        mergedSettings = {
+          ...defaultSettings,
+          ...settings['absolute-imports'],
+        }
+      }
+
+      let baseUrl;
+
+      if (mergedSettings.project) {
+        const baseDir = path.dirname(mergedSettings.project);
+
+        if (!baseDir) {
+          throw new Error(`Can not detect "baseUrl". Please check your configuration.`);
+        }
+
+        baseUrl = getBaseUrlFromConfig(baseDir, mergedSettings.project);
+      } else {
+        const baseDir = findDirWithFile("package.json");
+
+        if (!baseDir) {
+          throw new Error(`Can not detect "baseUrl". Please provide "project" in "settings" section.`);
+        }
+
+        baseUrl = getBaseUrl(baseDir);
+      }
+
+      if (!baseUrl) {
+        throw new Error(`Can not detect "baseUrl". Please check your configuration.`);
+      }
+
+      // Merge options
+      let userOptions = defaultOptions;
+
+      if (Array.isArray(options)) {
+        userOptions = options[0];
+      }
+
+      const mergedOptions = {
+        ...defaultOptions,
+        ...userOptions,
+      };
 
       return {
         ImportDeclaration(node) {
           const source = node.source.value;
+
+          if (mergedOptions && typeof mergedOptions === 'object') {
+            if (typeof mergedOptions.minLevel === 'number') {
+              if (mergedOptions.minLevel < 0) {
+                throw new Error(`"minLevel" must be greater than equal to 0`)
+              }
+
+              const levelMatches = source.match(/\.\.\//g);
+              const levelCount = levelMatches ? levelMatches.length : 0;
+
+              if (levelCount < mergedOptions.minLevel) return;
+            }
+
+            if (mergedOptions.jsOnly) {
+              if (HAS_EXT_REGEX.test(source) && !JS_EXT_REGEX.test(source)) return;
+            }
+          }
+
           if (source.startsWith(".")) {
             const filename = context.getFilename();
 
